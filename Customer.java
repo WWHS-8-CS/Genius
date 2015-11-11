79552960fc11efe4466ef7e656e67a5c9f332971
Q <Qrules@users.noreply.github.com>
2015-11-11 15:22:49 -0600

    Added base files, more work needed

---
+++ Customer.java
@@ -0,0 +1,28 @@
+
+public class Customer extends Genius{
+	private String name;
+	private String problem;
+	
+	public Customer(String name, String problem)
+	{
+		this.name = name;
+		this.problem = problem;
+	}
+	
+	public String returnName() 
+	{
+		return name;
+	}
+	public void changeName(String name) 
+	{
+		this.name = name;
+	}
+	public String returnProblem() 
+	{
+		return problem;
+	}
+	public void changeProblem(String problem) 
+	{
+		this.problem = problem;
+	}
+}
